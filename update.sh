8b0fce633bdf6895d5068e19ed66009121a65dde
lihang.gw <lihang.gw@heyqu.net>
2020-09-24 19:55:08 +0800

    开发：使用Travis打包

--- update.sh
+++ update.sh
@@ -1,18 +1,7 @@
-###
- # @Author: your name
- # @Date: 2020-09-22 09:42:32
- # @LastEditTime: 2020-09-24 19:25:31
- # @LastEditors: your name
- # @Description: In User Settings Edit
- # @FilePath: \blog_dashboard\update.sh
-### 
 #!/bin/bash
 
-echo "updating code..."
-git pull
-
-echo "building image..."
-docker build -t mrrs878/blog_dashboard:latest .
+echo "pulling image..."
+docker-compose pull
 
 echo "stoping app..."
 docker-compose down
