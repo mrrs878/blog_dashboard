e61c9062d1d7bfb89baa58295f4fe35a51782e91
mrrs878 <mrrs878@foxmail.com>
2021-01-17 18:00:19 +0800

    开发：添加EitableTagGroup

--- src/mock/article.ts
+++ src/mock/article.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-12 22:27:13
+ * @LastEditTime: 2021-01-17 16:01:48
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \blog_dashboard\src\mock\article.ts
+ */
 import { createMockRes } from '../tools';
 import ARTICLE_DATA from './json/article.json';
 
@@ -7,5 +15,5 @@ createMockRes<GetDictsReqT, GetDictsResT>(`${BASE_URL}`, 'get', () => ({
   success: true,
   code: 200,
   msg: '获取成功',
-  data: ARTICLE_DATA.article,
+  data: [],
 }));
--- src/views/mockApi/index.tsx
+++ src/views/mockApi/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { GET_MOCK_APIS } from '../../api/mockApi';
 import useRequest from '../../hooks/useRequest';
+import EditableTagGroup from './components/EditableTagGroup';
 
 const Index = () => {
   const [getting, getMockApiRes, getMockApi] = useRequest(GET_MOCK_APIS, false);
   return (
-    <div className="container">there is mockApi page</div>
+    <div className="container">
+      <EditableTagGroup tags={['1', '2', '3']} />
+    </div>
   );
 };
 
