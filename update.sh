be6e49edc7d25c04e02d99e952e3014c7081b869
lihang.gw <lihang.gw@heyqu.net>
2021-02-03 19:07:59 +0800

    开发：修改updade.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "updating code..."
-git pull
+# echo "updating code..."
+# git pull
 
-echo "installing packages..."
-yarn install
+# echo "installing packages..."
+# yarn install
 
-echo "building..."
-yarn build:prod
+# echo "building..."
+# yarn build:prod
 
-echo "building image..."
-docker build -t mrrs878/blog_dashboard:latest .
+# echo "building image..."
+# docker build -t mrrs878/blog_dashboard:latest .
 
 echo "stoping app..."
 docker-compose down
