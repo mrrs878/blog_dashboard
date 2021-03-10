bd61794bd3c2d2ca3d3d71e181bdb7a7d2c17aa2
lihang.gw <lihang.gw@heyqu.net>
2021-03-10 11:26:57 +0800

    ci: 修改打包脚本

--- update.sh
+++ update.sh
@@ -2,20 +2,20 @@
 ###
  # @Author: your name
  # @Date: 2021-03-05 17:33:59
- # @LastEditTime: 2021-03-10 10:23:00
+ # @LastEditTime: 2021-03-10 11:26:51
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /blog_dashboard/update.sh
 ### 
 
-echo "updating code..."
-git pull
+# echo "updating code..."
+# git pull
 
-echo "installing packages..."
-yarn install
+# echo "installing packages..."
+# yarn install
 
-echo "building..."
-yarn build:prod
+# echo "building..."
+# yarn build:prod
 
 # echo "building image..."
 # docker build -t mrrs878/blog_dashboard:latest .
