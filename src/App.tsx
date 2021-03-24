e87ffb6da73bfb6a449ea7940aec6cdf5593af34
mrrs878 <mrrs878@foxmail.com>
2021-03-24 22:22:53 +0800

    fix: 修复全局样式没有生效的bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-23 11:21:04
- * @LastEditTime: 2021-02-26 18:56:28
+ * @LastEditTime: 2021-03-24 22:21:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /components_library/src/App.tsx
@@ -10,6 +10,7 @@ import React, { useReducer } from 'react';
 import MErrorBoundary from './components/MErrorBoundary';
 import MLayout from './layout';
 import { defaultState, reducer, StoreContext } from './store';
+import './global.less';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, defaultState, undefined);
