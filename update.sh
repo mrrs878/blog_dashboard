f3b55bd394f547da198cd155f5ab238eeb12212f
mrrs878 <mrrs878@foxmail.com>
2021-07-20 23:48:57 +0800

    ci: 修改部署脚本

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 ###
  # @Author: mrrs878@foxmail.com
  # @Date: 2021-03-05 17:33:59
- # @LastEditTime: 2021-07-20 23:31:44
+ # @LastEditTime: 2021-07-20 23:48:38
  # @LastEditors: mrrs878@foxmail.com
  # @Description: In User Settings Edit
  # @FilePath: \blog_dashboard\update.sh
 ### 
 
-# echo "updating code..."
-# git pull
+echo "updating code..."
+git pull
 
 # echo "installing packages..."
 # yarn install
@@ -24,7 +24,7 @@ echo "stoping app..."
 docker-compose down
 
 echo "removing image(s)..."
-docker image rm blog_dashboard:master
+docker image rm mrrs878/blog_dashboard:master
 
 echo "restarting app..."
 docker-compose up -d --build
